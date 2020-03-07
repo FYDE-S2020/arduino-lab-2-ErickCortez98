a5efe1ed840f618957ad0e5bbe87e452ac9110e3
ErickCortez98 <erick-cortez98@utexas.edu>
2020-03-06 23:09:52 -0600

    Arduino Lab 2

--- BlynkStarter/BlynkStarter.ino
+++ BlynkStarter/BlynkStarter.ino
@@ -29,9 +29,13 @@
   Feel free to apply it to any other example. It's simple!
  *************************************************************/
 
+/*VO - Button
+ * V1 - Slider
+ * V2 - Terminal
+ */
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
-
+#define LED_PIN 2
 
 #include <WiFi.h>
 #include <WiFiClient.h>
@@ -39,24 +43,102 @@
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "Your token here";
+char auth[] = "FH8W2r0mgivm64LfDVMoFlUHqXcYy-cK";
+const int freq = 5000;     // 5KHz frequency is more than enough. Remember we used 100 before.
+const int ledChannel = 0;  // You can have up to 16 pwm channels (0 to 15)
+const int resolution = 10; // 10 bit resolution for 1023. Freq should be greater than resolution.
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
 // The EE IOT network is hidden. You might not be able to see it.
 // But you should be able to connect with these credentials. 
-char ssid[32] = "EE-IOT-Platform-02";
-char pass[32] = "g!TyA>hR2JTy";
+char ssid[32] = "Ball-Park-West";
+char pass[32] = "ballparkwest1600";
+int val = 0;
+int val2 = 1023; 
+int active = 0;
+int time_count = 0; // timer counter global variable
+String content = "";  // null string constant ( an empty string )
+BlynkTimer timer;
 
 void setup()
 {
   // Serial Monitor
   Serial.begin(115200);
   Blynk.begin(auth, ssid, pass);
+  pinMode(LED_PIN, OUTPUT);
+  // configure LED PWM functionality
+  ledcSetup(ledChannel, freq, resolution);
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(LED_PIN, ledChannel);
+  // Setup a function to be called every second
+  timer.setInterval(10L, myTimerEvent);
 }
 
 void loop()
 {
   Blynk.run();
+  timer.run(); // Initiates BlynkTimer
+  ledcWrite(ledChannel, val);
+}
+BLYNK_WRITE(1)
+{ 
+    // param is a member variable of the Blynk ADT. It is exposed so you can read it.
+    val2 = param.asInt(); // assigning incoming value from pin V1 to a variable
+    //LED changes brigthness only if button has made LED be on, otherwise LED doesn't turn on with changed brightness
+    if(active == 1){
+      val = val2;
+     }else{
+      val = 0;
+      }
+
+}
+BLYNK_WRITE(V0)
+{
+    Serial.println("Inside Button Function");
+    // param is a member variable of the Blynk ADT. It is exposed so you can read it.
+    int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+
+    // Because V0 is a button, pinValue will be a 0 or a 1.
+    // Led turns on and off when pressing and releasing the button 
+    if (pinValue == 1) {
+       // turn LED off
+       if(active == 0){
+        val = val2;
+        active = 1;
+        }else{
+          val = 0;
+          active = 0;
+        }
+    }
 }
 
+// This function sends Arduino's up time every second to Virtual Pin 0.
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+// You can send any value at any time.
+// Don't send more than 10 values a second or the Blynk server will block you!
+    if (time_count % 25 == 0) { // every 250 ms
+        // Do thing that needs to happen every 0.25 seconds
+    }
+    if (time_count == 100){
+        // Do thing that needs to happen every 1 second
+        Blynk.virtualWrite(V2, millis() / 1000); // Write the arduino uptime every second
+        time_count = 0; // reset time counter
+    }
+    else {
+        // Send serial data to Blynk terminal
+        char character;
+        while(Serial.available()) { // Check if serial is available every 10 ms
+            character = Serial.read();
+            content.concat(character);
+        }
+        if (content != "") {
+            Blynk.virtualWrite(V2, content);
+            content = ""; // Clear String
+        }  
+    }
+    time_count += 1; // Increment on every tick
+}
--- Report.md
+++ Report.md
@@ -1,43 +1,43 @@
-Name:
+Name: Erick Isaac Cortez Valdez
 
-EID:
+EID: eic366
 
-Team Number:
+Team Number: 14
 
 ## Questions
 
 1. What is the purpose of an IP address?
 
-    answer
+    Is the 'digital' address of a machine with capacity to connect to the internet and is used as an identifier for that machine
 
 2. What is a DNS? What are the benefits of using domain names instead of IP addresses?
 
-    answer
+    A DNS converts the 'friendly' name of a website into the corresponding IP address. We use domain addresses because they are much easier to remember for humans compared to IP addresses. 
 
 3. What is the difference between a static IP and a dynamic IP?
 
-    answer
+    A static IP address doesn't change with time while a dynamic IP does change over time.
 
 4. What is the tradeoff between UDP and TCP protocols?
 
-    answer
+    TCP is more reliable because it provides error checking and the delivery of the data, while UDP is faster because it doesn't provide extensive error checking. 
 
 5. Why can't we use the delay function with Blynk?
 
-    answer
+    Because Blynk is event based, meaning that if we have a delay and an event happens, the events wouldn' be read and the timing would break. 
 
 6. What does it mean for a function to be "Blocking"?
 
-    answer
+    It means that while the function is running, nothing else can run. 
 
 7. Why are interrupts useful for writing Non-Blocking code?
 
-    answer
+   Because whenever a new function with higher priority that the current one interrupts, the program takes the interrupt. Basically by having interrupts we ensure that functions do not take up time that other may need. 
 
 8. What is the difference between interface and implementation? Why is it important?
 
-   answer
+   Interface defines how a function is called while the implementantion is what the function actually does.
 
 9. Screenshot of your Blynk App:
 
-    ![your image here->](img/placeholder.png)
+    ![your image here->](img/ImageApp.jpg)
